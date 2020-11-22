4a19b1fef95e391f644dae9386f67cbc0d831092
TerryThreatt <terry.threatt@gmail.com>
2020-11-21 19:45:17 -0600

    add seed data

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -1,10 +1,8 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Employee.create(name: "Test1", email: "test1@test.com", password: "test1", employee_id: 1)
+
+Laptop.create(name: "Apple MacBook Pro", specs: "Apple M1 Chip (13-inch, 8GB RAM, 256GB SSD Storage) - Silver")
+Laptop.create(name: "Lenovo Ideapad S145 15", specs: "15.6 FHD Display I AMD Ryzen 3 3200U(> i5-7200U) I 8GB 256GB PCIe SSD I WiFi Webcam Dolby Audio 4-in-1 Card Reader Win 10 + Delca 16GB Micro SD Card")
+Laptop.create(name: "HP Spectre x360", specs: "13.3 4K UHD Touchscreen Intel Quad-Core i5-1035G4 (Beats i7-7500U) 8GB DDR4 256GB")
 
 
-Employee.create(name: "Test1", email: "test1@test.com", password: "test1")
+Rental.create(request_date: DateTime.now(), employee_id: 1, laptop_id: 1)
