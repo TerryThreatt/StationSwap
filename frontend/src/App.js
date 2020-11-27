9d8263c341892b693c6092a439ad207ac99f372f
TerryThreatt <terry.threatt@gmail.com>
2020-11-27 14:33:23 -0600

    add dashboard

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,11 @@
 import './App.css';
+import Dashboard from './components/Dashboard.js'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Hello World!
-        </p>
+        <Dashboard />
       </header>
     </div>
   );
