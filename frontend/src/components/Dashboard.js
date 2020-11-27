de0a767df7ee34bfd3518b151486ce258279f42b
TerryThreatt <terry.threatt@gmail.com>
2020-11-27 14:37:27 -0600

    add navbar

--- frontend/src/components/Dashboard.js
+++ frontend/src/components/Dashboard.js
@@ -1,8 +1,11 @@
 import React from 'react'
+import Navbar from './Navbar'
 
 const Dashboard = () => {
     return (
         <div>
+            <Navbar />
+            <br/>
             Dashboard
         </div>
     )
