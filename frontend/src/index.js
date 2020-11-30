8f2effe0a0d8de88a4bd0cf24433ed5f0b7c869d
TerryThreatt <terry.threatt@gmail.com>
2020-11-30 11:27:04 -0600

    asetup & configured redux

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,11 +4,32 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css'
+import { createStore, applyMiddleware, combineReducers } from 'redux'
+import { Provider } from 'react-redux'
+import thunk from 'redux-thunk'
+import { BrowserRouter as Router } from 'react-router-dom'
+import employeeReducer from './reducers/employeeReducer'
+import laptopReducer from './reducers/laptopReducer'
+import rentalReducer from './reducers/rentalReducer'
+
+let rootReducer = combineReducers({
+  employees: employeeReducer,
+  laptops: laptopReducer,
+  rentals: rentalReducer
+})
+
+let enhancers = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+
+let store = createStore(rootReducer, enhancers, applyMiddleware(thunk))
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <Router>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </Router>
+  </Provider>,
   document.getElementById('root')
 );
 
